7a473fcac6ab2bb7fad35d928b744cbb9923591e
Njabulo Hope Makana <makanahopenjabulo@gmail.com>
2026-08-09 12:27:13 +0200

    Create Account.cs

---
+++ BluePeakBank.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BluePeakBank.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BluePeakBank.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
---
+++ BluePeakBank.Web/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace BluePeakBank.Web.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public DateTime AccountDate {get; set;}
+    }
+}

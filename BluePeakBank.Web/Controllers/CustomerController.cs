58127ef504974328ec29bc00bc6c5a8962565972
Njabulo Hope Makana <makanahopenjabulo@gmail.com>
2026-08-09 11:36:39 +0200

    Create Bank Controllers

---
+++ BluePeakBank.Web/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BluePeakBank.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BluePeakBank.Web/Controllers/LoanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BluePeakBank.Web.Controllers
+{
+    public class LoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
